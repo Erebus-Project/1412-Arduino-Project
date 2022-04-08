9e1d360b63ab6ec02630a7fecd0ba8218f593ae7
Erebus-Project <73256533+Erebus-Project@users.noreply.github.com>
2022-04-08 09:56:50 -0500

    Update BlindTech.ino

--- BlindTech/BlindTech.ino
+++ BlindTech/BlindTech.ino
@@ -7,15 +7,35 @@
 #define FRpingPin 2 //Front Trigger pin
 #define REechoPin 3 //Back echo pin
 #define REpingpin 5 //Back ping pin
+#define IRLeft 10   //IR out pin left
+#define IRRight 8   //IR out pin right
+#define VMFront A0  //Front Vibrator motor pin
+#define VMBack A1   //Back Vibrator motor pin
+#define VMLeft A2   //Left Vibrator motor pin
+#define VMRight A3  //Right Vibrator motor pin
 
 bool DEBUG = false;
 
 void setup()
 {
   Serial.begin(9600);
+  //Ultrasonic Sensor pin set up
   pinMode(FRpingPin, INPUT);
   pinMode(FRechoPin, INPUT);
-  pinMode(A0, OUTPUT);
+  
+  pinMode(REpingPin, INPUT);
+  pinMode(REechoPin, INPUT);
+
+  //IR Sensor pin set up
+  pinMode(IRLeft, INPUT);   //Need LED Pin
+  
+  pinMode(IRRight, INPUT);  //Need LED Pin
+  
+  //Vibrator motor Pin set up
+  pinMode(VMFront, OUTPUT);
+  pinMode(VMBack, OUTPUT);
+  pinMode(VMLeft, OUTPUT);
+  pinMode(VMRight, OUTPUT);
 }
 
 void loop()
