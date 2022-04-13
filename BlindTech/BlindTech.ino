1ec5bb5477e3590a063a7f1f8369b58bb4f41a00
Erebus-Project <73256533+Erebus-Project@users.noreply.github.com>
2022-04-13 14:09:01 -0500

    Update BlindTech.ino

--- BlindTech/BlindTech.ino
+++ BlindTech/BlindTech.ino
@@ -1,16 +1,19 @@
 /*
  * Author: Caleb Lochner
- * Version 0.0.9 A
+ * Version 0.1.0 A
  */
 
 #define FRechoPin 4 //Front echo pin
 #define FRpingPin 2 //Front Trigger pin
+
 #define REechoPin 5 //Back echo pin
 #define REpingPin 3 //Back Trigger pin
-#define LechoPin 6  //Left echo pin
-#define LPingPin 10 //Left Trigger pin
 
-#define RPingPin 8   //Right ping pin
+#define LechoPin 6  //Left echo pin
+#define LPingPin 7  //Left Trigger pin
+
+#define RechoPin 8  //Right echo pin
+#define RPingPin 9   //Right Trigger pin
 
 #define VMFront A0  //Front Vibrator motor pin
 #define VMBack A1   //Back Vibrator motor pin
@@ -31,10 +34,11 @@ void setup()
   pinMode(REpingPin, INPUT);
   pinMode(REechoPin, INPUT);
 
-  //IR Sensor pin set up
-  pinMode(IRLeft, INPUT);   //Need LED Pin
+  pinMode(LPingPin, INPUT);
+  pinMode(LechoPin, INPUT);
   
-  pinMode(IRRight, INPUT);  //Need LED Pin
+  pinMode(RPingPin, INPUT);
+  pinMode(RechoPin, INPUT);
   
   //Vibrator motor Pin set up
   pinMode(VMFront, OUTPUT);
@@ -49,7 +53,9 @@ void loop()
   
   long cmFR = detectFR();   //Distance from front
   long cmRE = detectRE();   //Distance from Rear
-
+  long cmLE = detectLE();   //Distance From Left
+  long cmRI = detectRI();   //Distance from Right
+  
   if(cmFR < 40)             //Buzz Front on/off trigger
   {
     analogWrite(VMFront, buzzlevel(cmFR));
@@ -66,7 +72,25 @@ void loop()
   else
   {
     analogWrite(VMBack, 0);
-  }  
+  }
+
+  if(cmLE < 40)             //Buzz Left on/off trigger
+  {
+    analogWrite(VMLeft, buzzlevel(cmLE));
+  }
+  else
+  {
+    analogWrite(VMLeft, 0);
+  }
+
+  if(cmRI < 40)             //Buzz Right on/off trigger
+  {
+    analogWrite(VMRight, buzzlevel(cmRI));
+  }
+  else
+  {
+    analogWrite(VMRight, 0);
+  }
 }
 
 long detectFR()           //Front detect distance
@@ -96,6 +120,40 @@ long detectRE()           //Rear detect distance
   delayMicroseconds(10);
   digitalWrite(REpingPin, LOW);
   duration = pulseIn(REechoPin, HIGH);
+  cm = microsecondsToCentimeters(duration);
+  Serial.print("Back: ");
+  Serial.print(cm);
+  Serial.println(" cm");
+  return cm;
+}
+
+long detectLE()           //Left detect distance
+{
+  long duration, cm;
+  pinMode(LPingPin, OUTPUT);
+  digitalWrite(LPingPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(LPingPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(LPingPin, LOW);
+  duration = pulseIn(LechoPin, HIGH);
+  cm = microsecondsToCentimeters(duration);
+  Serial.print("Back: ");
+  Serial.print(cm);
+  Serial.println(" cm");
+  return cm;
+}
+
+long detectRI()           //Right detect distance
+{
+  long duration, cm;
+  pinMode(RPingPin, OUTPUT);
+  digitalWrite(RPingPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(RPingPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(RPingPin, LOW);
+  duration = pulseIn(RechoPin, HIGH);
   cm = microsecondsToCentimeters(duration);
   Serial.print("Back: ");
   Serial.print(cm);
